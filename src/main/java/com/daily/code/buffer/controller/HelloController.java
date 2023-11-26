3a36e6c35384ed4a21b1db40a8ac2c02cdf964db
Manish Singh <manishsinghadhikari44@gmail.com>
2023-11-26 23:02:11 +0530

    initial commit jenkins project

---
+++ src/main/java/com/daily/code/buffer/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.daily.code.buffer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String hello()
+	{
+		return "Hello World Controller";
+	}
+}
